d40576f4933f3fe90331e2874250bb2820e8ff4d
hamrachi <hamrachi@inception.myguest.virtualbox.org>
2025-12-11 01:12:48 +0100

    Few fixes. --Need to add env--

--- inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+
 set -e
 
-mkdir -p /run/php
-
-if [ ! -f /var/www/html/wp-config.php ] && [ ! -f /var/www/html/index.php ]; then
-  echo "Downloading WordPress..."
-  wget -qO /tmp/wordpress.tar.gz https://wordpress. org/latest.tar.gz
-  tar -xzf /tmp/wordpress.tar.gz -C /tmp
-  cp -R /tmp/wordpress/* /var/www/html/
-  chown -R www-data:www-data /var/www/html
-  rm -rf /tmp/wordpress /tmp/wordpress.tar.gz
+if [ ! -f "/usr/local/bin/wp" ]; then
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
 fi
 
-exec "$@"
+cd /var/www/html
+
+if [ ! -f "wp-confing.php" ]; then
+	wp core download --allow-root
+
+	wp config create --dbname=MYSQ --dbuser= --dbpass= --dbhost= --allow-root
+
+	wp core install --url= --title= --admin_user= --admin_password= --admin_email= --skip-email --allow-root
+
+	wp user create "wp_user" "wp_mail" --role= --user_pass= --allow-root
+fi
+
+exec php-fpm7.4 -F
