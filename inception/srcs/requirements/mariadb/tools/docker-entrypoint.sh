fa8e73ecebb0276fad1fccabe902aca95e14dc02
hamrachi <hamrachi@inception.myguest.virtualbox.org>
2025-12-11 01:42:17 +0100

    New env

--- inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-set -e
-
-service start mariadb
+service mariadb start
 
 sleep 5
 
-DB_ROOT_PASS=$(cat /run/secrets/db_root_password)
-DB_PASS=$(cat /run/secrets/db_password)
-
-mysql -u root <<-EOSQL
-  CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-  CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-  GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-  FLUSH PRIVILEGES;
+mariadb <<-EOSQL
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
 EOSQL
 
-mysqladmin shutdown
+mysqladmin  shutdown
 
-exec mysqld --user=mysql --port=3306 --bind-address=0.0.0.0
+exec mariadbd --port=3306 --datadir=/var/lib/mysql
--- inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -10,14 +10,14 @@ fi
 
 cd /var/www/html
 
-if [ ! -f "wp-confing.php" ]; then
+if [ ! -f "wp-config.php" ]; then
 	wp core download --allow-root
 
-	wp config create --dbname=MYSQ --dbuser= --dbpass= --dbhost= --allow-root
+	wp config create --dbname=$"MYSQL_DATABASE" --dbuser=$"MYSQL_USER" --dbpass=$"MYSQL_PASSWORD" --dbhost=mariadb --allow-root
 
-	wp core install --url= --title= --admin_user= --admin_password= --admin_email= --skip-email --allow-root
+	wp core install --url=$"DOMAIN_NAME" --title=$"WP_TITLE" --admin_user=$"WP_ADMIN_USER" --admin_password=$"WP_ADMIN_PASSWORD" --admin_email=$"WP_ADMIN_EMAIL" --skip-email --allow-root
 
-	wp user create "wp_user" "wp_mail" --role= --user_pass= --allow-root
+	wp user create "$"WP_USER"" "$"WP_USER_EMAIL"" --role=author --user_pass=$"WP_USER_PASSWORD" --allow-root
 fi
 
 exec php-fpm7.4 -F
