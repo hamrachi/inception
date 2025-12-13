91126853d6e37b9442fe5144782637195fe51023
hamrachi <hamrachi@hamrachi.hamrachi>
2025-12-12 21:33:21 -0500

    Fix MariaDB restart loop

--- inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -5,15 +5,20 @@ service mariadb start
 
 sleep 5
 
-mariadb -h localhost <<-EOSQL
-    FLUSH PRIVILEGES;
-    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    FLUSH PRIVILEGES;
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+    
+    mariadb -u root <<-EOSQL
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+        FLUSH PRIVILEGES;
 EOSQL
 
-mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    
+else
+    mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+fi
 
 exec mariadbd-safe --bind-address=0.0.0.0
